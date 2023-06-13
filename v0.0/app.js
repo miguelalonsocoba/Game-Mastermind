3e5cdf54b2306a0cc94fcd6d2022a122d202a83e
Miguel Alonso <miguetheking53@gmail.com>
2023-06-13 13:59:30 -0600

    Adding color placement in the correct position

--- v0.0/app.js
+++ v0.0/app.js
@@ -2,36 +2,91 @@ const { Console } = require(`console-mpds`);
 const console = new Console();
 
 let columnProposedCombinations = [
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
+  [`Na`, `Na`, `Na`, `Na`],
+  [`Na`, `Na`, `Na`, `Na`],
+  [`Na`, `Na`, `Na`, `Na`],
+  [`Na`, `Na`, `Na`, `Na`],
+  [`Na`, `Na`, `Na`, `Na`],
+  [`Na`, `Na`, `Na`, `Na`],
+  [`Na`, `Na`, `Na`, `Na`],
 ];
+let columnResult = [
+  [`Na`, `Na`, `Na`, `Na`],
+  [`Na`, `Na`, `Na`, `Na`],
+  [`Na`, `Na`, `Na`, `Na`],
+  [`Na`, `Na`, `Na`, `Na`],
+  [`Na`, `Na`, `Na`, `Na`],
+  [`Na`, `Na`, `Na`, `Na`],
+  [`Na`, `Na`, `Na`, `Na`],
+];
+const COLOR_OPTIONS = [
+  [`0`, `RED`],
+  [`1`, `GREEN`],
+  [`2`, `BLUE`],
+  [`3`, `YELLOW`],
+  [`4`, `CYAN`],
+  [`5`, `MAGENTA`],
+];
+let counterOfProposedCombination = 6;
+let repeatedColor = false;
 
-play();
+do {
+  for (let i = 0; i < columnProposedCombinations[0].length; i++) {
+    showBoard();
+    // let optionOfColor = chooseOptionOfColor();
+    // let optionOfPosition = setColorInPorposedCombination();
+    placeColorInPosition(chooseOptionOfColor(), setColorInPorposedCombination());
+    console.writeln(columnProposedCombinations);
+  }
+  counterOfProposedCombination--;
+} while (true);
 
-function play() {
-  /**Show board:
-   * 1- Show result column.
-   * 2- Show proposed combination.
-   * 3- Show secret combination.
-   * */
-  displayBoard();
-  chooseOption();
+function placeColorInPosition(color, position) {
+  if (columnProposedCombinations[counterOfProposedCombination][position] === `Na`) {
+    for (let i = 0; i < columnProposedCombinations[counterOfProposedCombination].length; i++) {
+      if (columnProposedCombinations[counterOfProposedCombination][i] === color) {
+        repeatedColor = true;
+      }
+      if (!repeatedColor) {
+        columnProposedCombinations[counterOfProposedCombination][position] = color;
+      }
+    }
+  }
 }
 
-function displayBoard() {}
+function setColorInPorposedCombination() {
+  let position = console.readNumber(`Select the position to place the chosen color:`);
+  return position;
+}
 
-function chooseOption() {
-  const selectedOption = console.readNumber(`Select color:
-    1 -> RED.
-    2 -> GREEN.
-    3 -> BLUE.
-    4 -> YELLOW.
-    5 -> CYAN.
-    6 -> MAGENTA.`);
-  return selectedOption;
+function showBoard() {
+  showMainPanel(columnResult, columnProposedCombinations);
+  showSecreteCombination();
+}
+
+function showMainPanel(columnResult, columnProposedCombinations) {
+  console.write(`Proposed\nCombinations: `);
+  console.writeln(` Results:`);
+  for (let i = 0; i < columnProposedCombinations.length; i++) {
+    console.write(`${columnProposedCombinations[i]}  |  `);
+    console.writeln(`${columnResult[i]}`);
+  }
+}
+
+function chooseOptionOfColor() {
+  let msg = `Select the color number:\n`;
+  for (let i = 0; i < COLOR_OPTIONS.length; i++) {
+    msg += `${COLOR_OPTIONS[i][0]} -> ${COLOR_OPTIONS[i][1]}\n`;
+  }
+  const selectedOption = console.readString(msg);
+  for (let i = 0; i < COLOR_OPTIONS.length; i++) {
+    if (COLOR_OPTIONS[i][0] === selectedOption) {
+      console.writeln(`Color Value: ${COLOR_OPTIONS[i][1]}`);
+      return COLOR_OPTIONS[i][1];
+    }
+  }
+}
+
+function showSecreteCombination() {
+  console.writeln(`Secrete combination: 1502`);
 }
